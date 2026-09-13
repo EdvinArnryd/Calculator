632c8c70fbe18102503dc77c2ece4c759581f44b
Edvin Arnryd <edvin.arn@hotmail.com>
2026-09-13 22:37:34 +0200

    Created css for calculator

--- script.js
+++ script.js
@@ -1,2 +1,41 @@
-const container = document.querySelector(".container");
-container.textContent = "HELLO";
+
+let firstNum = 0;
+let secondNum = 0;
+let operator = "";
+
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+function operate(a, b, op)
+{
+    switch(op)
+    {
+        case "+":
+            console.log(add(a,b));
+            break;
+        case "-":
+            console.log(subtract(a,b));
+            break;
+        case "*":
+            console.log(multiply(a,b));
+            break;
+        case "/":
+            console.log(divide(a,b));
+            break;
+        default:
+            break;
+    }
+}
